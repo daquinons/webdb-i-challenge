fa16eb2b65c5869c2350db4de0277cd2b4035d83
David Quiñones <daquinons@users.noreply.github.com>
2019-07-15 13:54:24 +0200

    Add routes for accounts

--- services/accounts/routes.js
+++ services/accounts/routes.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/users', (req, res, next) => {
-  res.json({ message: 'Get all users' });
+router.get('/accounts', (req, res, next) => {
+  res.json({ message: 'Get all accounts' });
+});
+
+router.post('/accounts', (req, res, next) => {
+  res.json({ message: 'Create account' });
+});
+
+router.get('/accounts/:id', (req, res, next) => {
+  res.json({ message: 'Get account with ' + req.params.id });
+});
+
+router.put('/accounts/:id', (req, res, next) => {
+  res.json({ message: 'Update account ' + req.params.id });
+});
+
+router.delete('/accounts/:id', (req, res, next) => {
+  res.json({ message: 'Delete account ' + req.params.id });
 });
 
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
+const accountsRoutes = require('./accounts/routes');
+
 router.get('/api', (req, res, next) => {
-  res.json({ message: "API is up"});
+  res.json({ message: 'API is up' });
 });
+
+router.use('/api', accountsRoutes);
 
 module.exports = router;
